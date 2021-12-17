17719a4024387ac356943ed4173039cd18cba6e8
Георгий Владимирович Барышников <anny1280008@gmail.com>
2021-12-17 10:37:30 +0300

    Добаление кнопок главного управления

---
+++ TestLogic/MVVM/ViewModel/Commands/Close.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace TestLogic.MVVM.ViewModel.Commands
+{
+    class Close : Command
+    {
+        public override bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public override void Execute(object parameter) => DisplayMesseg();
+
+        public void DisplayMesseg()
+        {
+            var res = 5+5;
+            MessageBox.Show(res.ToString());
+        }
+    }
+}
